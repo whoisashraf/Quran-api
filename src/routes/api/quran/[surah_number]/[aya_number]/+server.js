0094ea78bef0cd808ae8c8b69ec660cabd781790
whoisashraf <124151689+whoisashraf@users.noreply.github.com>
2023-09-16 20:13:17 +0100

    1 file modified

--- src/routes/api/quran/[surah_number]/[aya_number]/+server.js
+++ src/routes/api/quran/[surah_number]/[aya_number]/+server.js
@@ -1,24 +1,9 @@
 import { json, error } from '@sveltejs/kit';
 import quranData from '/static/data/merged_data.json'; // Replace with the path to your merged data JSON file
 
-//  Endpoint 2: Get Specific Surah aya
-// export async function GET({ params }) {
-// 	try {
-// 		const aya_number = params.aya_number;
-// 		const aya = quranData.filter((s) => s.aya_no === parseInt(aya_number));
-// 		if (!aya) {
-// 			return error(404, 'Surah not found');
-// 		}
-// 		return json(aya);
-// 	} catch (err) {
-// 		return error(500, 'Internal Server Error');
-// 	}
-// }
-
 // Endpoint 3: Get Specific Ayah Data
 export async function GET({ params }) {
 	try {
-		// const { sura_no, aya_no } = params;
 		const sura_no = params.surah_number;
 		const aya_no = params.aya_number;
 		console.log(params);
