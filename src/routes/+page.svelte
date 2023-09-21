9536138e6e26e4e9b0cde55315a9f8d133ec3180
whoisashraf <124151689+whoisashraf@users.noreply.github.com>
2023-09-21 16:03:21 +0100

    7 files modified, 4 added, 1 deleted

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,70 @@
-<h1>Landing page</h1>
+<!-- Hero -->
+<div class="bg-slate-900">
+	<div class="bg-gradient-to-b from-violet-600/[.15] via-transparent">
+		<div class="mx-auto max-w-[1200px] space-y-8 px-4 py-24 sm:px-6 lg:px-8">
+			<!-- Announcement Banner -->
+			<!-- <div class="flex justify-center">
+          <a class="group inline-block rounded-full border border-white/[.05] bg-white/[.05] p-1 pl-4 shadow-md hover:bg-white/[.1]" href="../docs">
+            <p class="mr-2 inline-block text-sm text-white">Read the docs!</p>
+            <span class="inline-flex items-center justify-center gap-x-2 rounded-full bg-white/[.075] px-3 py-2 text-sm font-semibold text-white group-hover:bg-white/[.1]">
+              <svg class="h-2.5 w-2.5" width="16" height="16" viewBox="0 0 16 16" fill="none">
+                <path d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14" stroke="currentColor" stroke-width="2" stroke-linecap="round" />
+              </svg>
+            </span>
+          </a>
+        </div> -->
+			<!-- End Announcement Banner -->
+
+			<!-- Title -->
+			<div class="mx-auto max-w-3xl text-center">
+				<h1 class="block text-4xl font-medium text-gray-200 sm:text-5xl md:text-6xl lg:text-7xl">
+					Al-Quran Api
+				</h1>
+			</div>
+			<!-- End Title -->
+
+			<div class="mx-auto max-w-3xl text-center">
+				<p class="text-lg text-gray-400">
+					Al-Quran Api is an open source api for the Holy Book, translation(Saheeh International)
+					and Uthmani font included :)
+				</p>
+			</div>
+
+			<!-- Buttons -->
+			<div class="text-center">
+				<a
+					class="inline-flex items-center justify-center gap-x-3 rounded-full border border-transparent bg-gradient-to-tl from-blue-600 to-violet-600 px-3 py-3 text-center text-sm font-medium text-white shadow-lg shadow-transparent hover:shadow-blue-700/50 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-white dark:focus:ring-offset-gray-800"
+					href="/docs"
+				>
+					Get started
+					<svg class="h-2.5 w-2.5" width="16" height="16" viewBox="0 0 16 16" fill="none">
+						<path
+							d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14"
+							stroke="currentColor"
+							stroke-width="2"
+							stroke-linecap="round"
+						/>
+					</svg>
+				</a>
+			</div>
+			<!-- End Buttons -->
+
+			<!-- Features -->
+			<div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+				<!-- Title -->
+				<div class="mx-auto max-w-2xl mb-8 lg:mb-14 text-center">
+					<h2 class="text-3xl lg:text-4xl text-gray-300 font-bold dark:text-gray-200">
+						Free and Open
+					</h2>
+					<p class="mt-3 text-gray-400 dark:text-gray-200">
+						The Api's are powered by data from tanzil.net for translations and qurancomplex.gov.sa. <br
+						/> Get the full dump or get them by surahs!
+					</p>
+				</div>
+				<!-- End Title -->
+			</div>
+			<!-- End Features -->
+		</div>
+	</div>
+</div>
+<!-- End Hero -->
--- src/routes/font/+page.svelte
+++
@@ -1,2 +0,0 @@
-<h1>Font download</h1>
-<a href="/static/fonts/uthmanic_hafs_v20.ttf" download><button>Download</button></a>
